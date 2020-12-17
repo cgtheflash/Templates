374eb5d62e4a3f8044fb18b44f6fb7c51d30d590
cgtheflash <66049050+cgtheflash@users.noreply.github.com>
2020-12-17 11:31:22 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,9 @@ apt-get -y install mysql-server
 apt-get -y install php libapache2-mod-php php-mysql
 
 # write HTML file
-echo \<html\>\<body style=\"background-color: black;\"\>\<center\>\<a href=\"https://www.trace3.com\"\>\<img src=\"https://www.trace3.com/media/logo/websites/6/Trace3_logo_RGB.png\" alt=\"Trace 3 Logo\"\>\</a\>\<h1 style=\"color: white\">Demo App\</h1>\<br/\>\</center\>\</html\> > /var/www/html/index.html
+cat << 'EOF' > /var/www/html/index.html 
+<html><body style="background-color: black;"><center><a href="https://www.trace3.com"><img src="https://www.trace3.com/media/logo/websites/6/Trace3_logo_RGB.png" alt="Trace 3 Logo"></a><h1 style="color: white">Demo App</h1><br/></center></html> 
+EOF
 
 # restart Apache
 apachectl restart
